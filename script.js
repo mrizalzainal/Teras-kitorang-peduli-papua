5909041f456b205ce4a8317c3637250dec842856
mrizalzainal111@gmail.com <mrizalzainal111@gmail.com>
2023-07-12 00:19:33 +0900

    mengaktifkan tombol search

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-//toggle class active
+//toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+//toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
 };
 
 //click di luar sidebar untuk menghilangkan nav
 
-const hamburger = document.querySelector("#hamburger-menu");
+const hb = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hb.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+//click di luar sidebar untuk menghilangkan search form
+
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
